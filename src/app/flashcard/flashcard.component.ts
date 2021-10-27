f7da8278dccdfbaa4fee039d887f438189a1f488
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-27 20:43:41 +0300

    add filters to the subjects

--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -4,19 +4,86 @@ import { Flashcard } from '../flashcard';
 @Component({
   selector: 'app-flashcard',
   templateUrl: './flashcard.component.html',
-  styleUrls: ['./flashcard.component.css']
+  styleUrls: ['./flashcard.component.css'],
 })
 export class FlashcardComponent implements OnInit {
+  allflashcards = [
+    new Flashcard(
+      1,
+      'Lakes',
+      'We have then returned the days left if the goal date has not been passed',
+      'Geography',
+      new Date(2021, 9, 27),
+      new Date(2021, 9, 27)
+    ),
+    new Flashcard(
+      2,
+      'Prepositions',
+      'We have then returned the days left if the goal date has not been passed',
+      'English',
+      new Date(2021, 9, 26),
+      new Date(2021, 9, 27)
+    ),
+    new Flashcard(
+      3,
+      'Matrices',
+      'We have then returned the days left if the goal date has not been passed',
+      'Mathematics',
+      new Date(2021, 9, 25),
+      new Date(2021, 9, 26)
+    ),
+    new Flashcard(
+      4,
+      'Pollination',
+      'We have then returned the days left if the goal date has not been passed',
+      'Biology',
+      new Date(2021, 9, 24),
+      new Date(2021, 9, 25)
+    ),
+  ];
   flashcards = [
-    new Flashcard(1, 'Lakes', 'We have then returned the days left if the goal date has not been passed', 'Geography', new Date(2021,9, 27), new Date(2021,9, 27)),
-    new Flashcard(2, 'Prepositions', 'We have then returned the days left if the goal date has not been passed', 'English', new Date(2021,9, 26), new Date(2021,9, 27)),
-    new Flashcard(3, 'Matrices', 'We have then returned the days left if the goal date has not been passed', 'Mathematics', new Date(2021,9, 25), new Date(2021,9, 26)),
-    new Flashcard(4, 'Pollination', 'We have then returned the days left if the goal date has not been passed', 'Biology', new Date(2021,9, 24), new Date(2021,9, 25)),
-]
- 
-  constructor() { }
+    new Flashcard(
+      1,
+      'Lakes',
+      'We have then returned the days left if the goal date has not been passed',
+      'Geography',
+      new Date(2021, 9, 27),
+      new Date(2021, 9, 27)
+    ),
+    new Flashcard(
+      2,
+      'Prepositions',
+      'We have then returned the days left if the goal date has not been passed',
+      'English',
+      new Date(2021, 9, 26),
+      new Date(2021, 9, 27)
+    ),
+    new Flashcard(
+      3,
+      'Matrices',
+      'We have then returned the days left if the goal date has not been passed',
+      'Mathematics',
+      new Date(2021, 9, 25),
+      new Date(2021, 9, 26)
+    ),
+    new Flashcard(
+      4,
+      'Pollination',
+      'We have then returned the days left if the goal date has not been passed',
+      'Biology',
+      new Date(2021, 9, 24),
+      new Date(2021, 9, 25)
+    ),
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  filterCards(subject: string) {
+    this.flashcards = this.allflashcards;
+    this.flashcards = this.allflashcards.filter(
+      (card) => card.subject == subject
+    );
   }
-
 }
